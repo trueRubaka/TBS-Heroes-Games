b999977b7142a2e76917596f1e5a2489a14bef3c
alesei salnikov <x.salnikov@gmail.com>
2023-08-02 16:51:21 +0300

    куб передвигается по полю и центруется по центру клетки

--- Heroes Games/Assets/Scripts/UnitWarrior.cs
+++ Heroes Games/Assets/Scripts/UnitWarrior.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
+using TMPro;
+using UnityEditor;
 using UnityEngine;
 
 public class UnitWarrior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed;
+    [SerializeField] private Vector3 target;
+    private bool isMoving = false;
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                target = hit.transform.localPosition;
+                isMoving = true;
+                Debug.Log(target);
+
+            }
+        }
+        if (isMoving == true)
+        {
+            if (!Mathf.Approximately(transform.position.magnitude, target.magnitude))
+            {               
+                transform.position = Vector3.Lerp(transform.position, target, 1 / (speed * (Vector3.Distance(transform.position, target))));
+            }           
+        }    
     }
 }
